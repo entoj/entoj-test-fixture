893abfc3e6d404b633ba5781411877786677c80f
Christian Auth <c.auth@syzygy.de>
2017-06-19 09:55:48 +0200

    feat(g-teaserlist): adds js file

---
+++ sites/base/module-groups/g-teaserlist/js/g-teaserlist.js
@@ -0,0 +1,27 @@
+
+/**
+ * Requirements
+ */
+import { Component as BaseComponent } from 'global/component';
+
+/**
+ * @memberOf base.modulegroup.g-teaserlist
+ */
+export class Component extends BaseComponent {
+
+    /* Constants ------------------------------------------------------------------------------- */
+    /* Constructor ----------------------------------------------------------------------------- */
+
+    /**
+     * @inheritDocs
+     */
+    static get className() {
+        return 'base.modulegroup.g-teaserlist/Component';
+    }
+
+
+    /* Lifecycle ------------------------------------------------------------------------------- */
+    /* Eventhandler ---------------------------------------------------------------------------- */
+    /* Protected ------------------------------------------------------------------------------- */
+    /* Public ---------------------------------------------------------------------------------- */
+}
