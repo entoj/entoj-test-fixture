c941d041967dfc2e273a2cdd7336888dec8642fc
Christian Auth <c.auth@syzygy.de>
2018-10-01 14:29:22 +0200

    chore: update configuration to match new organisation

--- index.js
+++ index.js
@@ -3,75 +3,78 @@
 /**
  * Configure
  */
-const configuration = {};
-
-
-// Global settings
-configuration.settings =
+const configuration =
 {
-    breakpoints:
+    system:
     {
-        mobile:
+        breakpoints:
         {
-            maxWidth: '320px'
+            mobile:
+            {
+                maxWidth: '320px'
+            },
+            phablet:
+            {
+                minWidth: '321px',
+                maxWidth: '767px'
+            },
+            miniTablet:
+            {
+                minWidth: '768px',
+                maxWidth: '1023px'
+            },
+            tablet:
+            {
+                minWidth: '1024px',
+                maxWidth: '1199px'
+            },
+            desktop:
+            {
+                minWidth: '1200px'
+            }
         },
-        phablet:
+        path:
         {
-            minWidth: '321px',
-            maxWidth: '767px'
+            base: __dirname,
+            sites: require('path').join(__dirname, 'sites'),
+            data: require('path').join(__dirname, 'data')
         },
-        miniTablet:
+        entity:
         {
-            minWidth: '768px',
-            maxWidth: '1023px'
-        },
-        tablet:
-        {
-            minWidth: '1024px',
-            maxWidth: '1199px'
-        },
-        desktop:
-        {
-            minWidth: '1200px'
+            categories: [
+                    {
+                        longName: 'Global',
+                        pluralName: 'Global',
+                        isGlobal: true,
+                        shortName: 'o'
+                    },
+                    {
+                        longName: 'Element'
+                    },
+                    {
+                        longName: 'Module'
+                    },
+                    {
+                        shortName: 'g',
+                        longName: 'Module Group'
+                    },
+                    {
+                        longName: 'Template'
+                    },
+                    {
+                        longName: 'Page'
+                    }
+                ]
         }
     }
 };
-
-
-// Some helpers
-const siteTemplate = '${site.name.urlify()}';
-const entityCategoryTemplate = siteTemplate + '/${entityCategory.pluralName.urlify()}';
-const entityIdTemplate = entityCategoryTemplate + '/${entityCategory.shortName.urlify()}-${entityId.name.urlify()}';
-
-
-// Urls
-configuration.urls =
-{
-    root: '',
-    siteTemplate: '${root}/' + siteTemplate,
-    sitePattern: '^${root}/(?<sites>[\\w\\d_]+)$',
-    entityCategoryTemplate: '${root}/' + entityCategoryTemplate,
-    entityIdTemplate: '${root}/' + entityIdTemplate
-};
-
-
-// Pathes
-configuration.pathes =
-{
-    root: __dirname,
-    cacheTemplate: '${root}/cache',
-    sitesTemplate: '${root}/sites',
-    siteTemplate: '${sites}/' + siteTemplate,
-    entityCategoryTemplate: '${sites}/' + entityCategoryTemplate,
-    entityIdTemplate: '${sites}/' + entityIdTemplate
-};
-
 
 /**
  * Export
  * @ignore
  */
 module.exports.configuration = configuration;
+module.exports.pathToBase = __dirname;
 module.exports.pathToSites = require('path').join(__dirname, 'sites');
 module.exports.pathToData = require('path').join(__dirname, 'data');
 module.exports.pathToLibraries = require('path').join(__dirname, 'libraries');
